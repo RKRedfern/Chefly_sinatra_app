46212fe9b0cdf484532a168d06e2fa6a9dda2cc9
RKRedfern <rkredfern@gmail.com>
2020-12-09 16:19:14 -0500

    fixed new post route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,39 @@
 class PostsController < ApplicationController
 
-    #see all posts 
+    #Read
     get '/posts' do
         @posts = Post.all
         erb :'posts/index'
     end
 
-    #see one post
-    get '/posts/:id' do
-        @post = Post.find(params[:id])
-        erb :"/posts/show"
+    
+    get '/posts/new' do
+        erb :'posts/new'
     end
 
+    post '/posts' do
+        @post = Post.create(title: params[:title], image_url: params[:image_url], 
+            description: params[:descrpition], user_id: current_user.id)
+            
+            redirect "/posts/#{@post.id}"
+    end
+    
+    
+    get '/posts/:id' do
+        
+        @post = Post.find(params[:id])
+        erb :'/posts/show'
+        #I need an instance variable wherever I need to access an id and leverage the information it contains. 
+        #it expands its scope so I can use it. 
 
-    #CREATE
-
+    end
+    
+end
+    
     #UPDATE
 
     #DELETE
     
-end
+
 
 #Every time a controller is created, you must plug it in in the config file
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ riley = User.create(email: "riley@riley.com", username: "Riley Redfern", image_u
 
 don = User.create(email: "don@don.com", username: "Don Mallory", image_url: "https://avatars1.githubusercontent.com/u/60760677?s=400&v=4", bio: "The best human person alive!", password: "pw")
 
-Post.create(title: "Angel Food Cake", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fsallysbakingaddiction.com%2Fangel-food-cake%2F&psig=AOvVaw26nBXG8eyIPj345P2uM95a&ust=1607441694653000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCU_oqZvO0CFQAAAAAdAAAAABAD", description: "A classic complexity", user_id: riley.id)
+Post.create(title: "Angel Food Cake", image_url: "https://cdn.sallysbakingaddiction.com/wp-content/uploads/2019/05/angel-food-cake-600x900.jpg", description: "A classic complexity", user_id: riley.id)
 
-Post.create(title: "Duck a L'Orange a L'Martini", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.crazyforcrust.com%2Forange-martini-recipe%2F&psig=AOvVaw1hvIXalFHjqX5KKgt_3ztF&ust=1607441714901000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKik05SZvO0CFQAAAAAdAAAAABAD", description: "A jazzy cocktail inspired by an unforgettable french dish.", user_id: don.id)
+Post.create(title: "Duck a L'Orange a L'Martini", image_url: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/29/17/enhanced/webdr13/original-20671-1448835407-3.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto&output-quality=auto&output-format=auto&downsize=360:*", description: "A jazzy cocktail inspired by an unforgettable french dish.", user_id: don.id)
